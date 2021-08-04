fa6cbadbc86a8a202c698bed5e816a37fedc420c
Geyanth <geyanth.2018@gmail.com>
2021-08-04 18:10:31 +0530

    Drum Kit

---
+++ index.js
@@ -0,0 +1,62 @@
+
+var numberOfButtons = document.querySelectorAll(".drum").length;
+
+for(var i=0; i < numberOfButtons; i++){
+    document.querySelectorAll(".drum")[i].addEventListener("click", function(){
+        
+        var buttonInnerHTML = this.innerHTML;
+        
+        makesound(buttonInnerHTML);
+        add_animation(buttonInnerHTML);
+    });
+}
+
+document.addEventListener("keypress", function(event){
+    makesound(event.key);
+    add_animation(event.key);
+})
+
+function makesound(letter){
+    switch(letter){
+        case "w":
+            var crash = new Audio("sounds/crash.mp3");
+            crash.play();
+            break;
+        case "a":
+            var kick_bass = new Audio("sounds/kick-bass.mp3");
+            kick_bass.play();
+            break;
+        case "s":
+            var snare = new Audio("sounds/snare.mp3");
+            snare.play();
+            break;
+        case "d":
+            var tom_1 = new Audio("sounds/tom-1.mp3");
+            tom_1.play();
+            break;
+        case "j":
+            var tom_2 = new Audio("sounds/tom-2.mp3");
+            tom_2.play();
+            break;
+        case "k":
+            var tom_3 = new Audio("sounds/tom-3.mp3");
+            tom_3.play();
+            break;
+        case "l":
+            var tom_4 = new Audio("sounds/tom-4.mp3");
+            tom_4.play();
+            break;
+        default: console.log(letter);
+    }
+
+}
+
+function add_animation(current_key){
+    var current_button = document.querySelector("." + current_key);
+
+    current_button.classList.add("pressed");
+
+    setTimeout(function(){
+        current_button.classList.remove("pressed");
+    }, 100);
+}
